530633460de5a13fa8f7ae848cc39fc4ef38ebf8
Relz <relz0071@gmail.com>
2026-07-05 18:58:16 +0300

    gui: ignore inherited GTK_THEME

--- src/gui/main.c
+++ src/gui/main.c
@@ -4,6 +4,8 @@
 int
 main(int argc, char **argv)
 {
+  g_unsetenv("GTK_THEME");
+
   g_autofree char *target_output_name = network_sidebar_target_output_from_environment();
   NetworkSidebarGuiApp *app = network_sidebar_gui_app_new(target_output_name);
   return network_sidebar_gui_app_run(app, argc, argv);
